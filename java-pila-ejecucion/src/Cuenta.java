c90f8208f7f33162914424e30fde9e72f5f65d01
Sergio Reinoso <slrs993@gmail.com>
2023-03-19 19:16:28 -0500

    Aplicacion de excepciones

--- java-pila-ejecucion/src/Cuenta.java
+++ java-pila-ejecucion/src/Cuenta.java
@@ -1,4 +1,7 @@
 
 public class Cuenta {
 
+	 void deposita() throws MiException {
+		 
+	 }
 }
--- java-pila-ejecucion/src/Flujo.java
+++ java-pila-ejecucion/src/Flujo.java
@@ -3,18 +3,21 @@ public class Flujo {
 	
 	public static void main(String[] args) {
 		System.out.println("Inicio main");
-		metodo1();
+		try {
+			metodo1();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("Fin main");
 	}
-	public static void metodo1(){
+	public static void metodo1() throws MiException{
 		System.out.println("Inicio metodo1");
 		metodo2();
 		System.out.println("Fin de metodo1");
 	}
-	public static void metodo2(){
+	public static void metodo2() throws MiException{
 		System.out.println("Inicio metodo2");
-		Cuenta cuenta = new Cuenta();
-		throw new ArithmeticException();
+		throw new MiException("Mi excepcion fue lanzada");
 	}
 	  
 }
